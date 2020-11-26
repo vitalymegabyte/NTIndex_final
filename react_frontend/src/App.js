4f415e2ba1352ef486b76e14d9eadbe72e180296
vitalymegabyte <admin@vitaly12.ru>
2020-11-26 18:59:19 +0300

    cleared something

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -4,8 +4,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Main from './components/main.component';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
-import SectionPage from './pages/section.component';
-import ProfessionPage from './pages/profession.component';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-vis/dist/style.css';
@@ -15,8 +13,6 @@ function App() {
     <Provider store={store}>
       <BrowserRouter>
         <Switch>
-          <Route path='/section/:sectionId' component={SectionPage} />
-          <Route path='/profession/:professionId' component={ProfessionPage} />
           <Route path='/' component={Main} />
         </Switch>
       </BrowserRouter>
--- react_frontend/src/components/main.component.js
+++ react_frontend/src/components/main.component.js
@@ -1,34 +1,12 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { setSections } from '../redux/sections/sections.actions';
-import { selectSections } from '../redux/sections/sections.selectors';
 import { createStructuredSelector } from 'reselect';
-import SectionsTable from './sections_table/sections_table.component';
 import { Tab, Tabs, Container } from 'react-bootstrap';
-import SectionStats from '../components/sections_stats/section_stats.component';
 
 const Main = ({ setSections }) => {
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_BASEURL}/section`)
-      .then((response) => {
-        console.log(response.data);
-        setSections(response.data);
-      });
-  }, [setSections]);
   return (
     <>
-      <Container>
-        <Tabs defaultActiveKey='jobless' id='uncontrolled-tab-example'>
-          <Tab eventKey='jobless' title='Данные'>
-            <SectionsTable />
-          </Tab>
-          <Tab eventKey='home' title='Статистика'>
-            <SectionStats />
-          </Tab>
-        </Tabs>
-      </Container>
     </>
   );
 };
